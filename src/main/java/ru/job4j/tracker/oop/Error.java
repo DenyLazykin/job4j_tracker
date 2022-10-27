3b3d6d21eeffe8e3c58ee97640ff8d7bcc39d9f9
Denis Lazykin <dlazykin856@gmail.com>
2022-10-27 14:25:18 +0300

    1.7. Конструктор [#173889 #249315]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public void printInfo() {
+        System.out.println("Есть ошибка: " + active);
+        System.out.println("Ее номер: " + status);
+        System.out.println("Сообщение для пользователя: " + message);
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public static void main(String[] args) {
+        Error error414 = new Error(true, 414, "Слишком длинный URL");
+        Error error404 = new Error(true, 404, "Файл не найден");
+        Error noError = new Error();
+        noError.printInfo();
+        error404.printInfo();
+        error414.printInfo();
+    }
+}
