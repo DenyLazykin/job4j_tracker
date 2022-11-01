85929113460c956f39c5678830704749165432f7
Denis Lazykin <dlazykin856@gmail.com>
2022-11-01 10:31:23 +0300

    1.2. Вызов метода объекта [#166085 #248945]. Переименовал название класса на верное

--- src/main/java/ru/job4j/oop/Musician.java
+++ src/main/java/ru/job4j/oop/Musician.java
@@ -1,12 +1,12 @@
 package ru.job4j.oop;
 
-public class Student {
+public class Musician {
     public void music(String lyrics) {
         System.out.println("I can sign a song : " + lyrics);
     }
 
     public static void main(String[] args) {
-        Student petya = new Student();
+        Musician petya = new Musician();
         String song = "I believe I can fly";
         petya.music(song);
     }
