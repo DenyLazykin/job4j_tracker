2176119c964b29a12fdc17e718c48c0d8ea08b5c
Denis Lazykin <dlazykin856@gmail.com>
2022-11-05 15:38:15 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #255020]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,6 +1,15 @@
 package ru.job4j.ex;
 
 public class FindEl {
+    /**
+     * Проверяет, что в списке есть ключ шаблона сообщения.
+     * Метод может выкинуть исключение ElementNotFoundException.
+     *
+     * @param value массив значений
+     * @param key   слово, которое ищем
+     * @return номер элемента в массиве
+     * @throws ElementNotFoundException может выкинуть исключение ElementNotFoundException
+     */
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
@@ -16,11 +25,43 @@ public class FindEl {
         return rsl;
     }
 
+    /**
+     * Отправляет сообщение, если ключ не входит в ключи запрещенных ключей.
+     *
+     * @param value  ключь
+     * @param abuses массив ключей запрещенных ключей
+     * @return true, если ключ не входит в ключи запрещенных ключей
+     * @throws ElementAbuseException Метод может выкинуть исключение ElementAbuseException.
+     */
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        if (value.equals(abuses)) {
+            return Boolean.parseBoolean(value);
+        }
+        throw new ElementAbuseException("Неверный элемент");
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+
     public static void main(String[] args) {
         String[] names = new String[]{"Petr", "Ivan", "Nik", "Vasiliy"};
         try {
-            indexOf(names, "Vladimir");
+            indexOf(names, "Petr");
         } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+        process(names, "Petr", names);
+        try {
+            sent("Petr", names);
+        } catch (ElementAbuseException e) {
             e.printStackTrace();
         }
     }
