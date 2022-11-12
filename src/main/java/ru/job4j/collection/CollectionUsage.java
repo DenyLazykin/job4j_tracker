38b29d166d9c57f5db7cd11597af177004a7981e
Denis Lazykin <dlazykin856@gmail.com>
2022-11-12 12:45:11 +0300

    Интерфейс Collection [#504820 #257920].Демонстрация работы методов size, contains, toArray, clear, isEmpty

--- src/main/java/ru/job4j/collection/CollectionUsage.java
+++ src/main/java/ru/job4j/collection/CollectionUsage.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 
 public class CollectionUsage {
@@ -9,18 +10,10 @@ public class CollectionUsage {
         collection.add("one");
         collection.add("two");
         collection.add("three");
-        for (String str : collection) {
-            System.out.println(str);
-        }
-        Collection<String> list = new ArrayList<>();
-        list.add("two");
-        for (String str : list) {
-            System.out.println(list);
-        }
-        collection.retainAll(list);
-        System.out.println("Вывод содержимого после удаления");
-        for (String str : collection) {
-            System.out.println(str);
-        }
+        System.out.println("Размер коллекции равен: " + collection.size());
+        System.out.println("Коллекция содержит элемент two: " + collection.contains("two"));
+        System.out.println("Содержимое в виде массива: " + Arrays.toString(collection.toArray()));
+        collection.clear();
+        System.out.println("Коллекция после очистки пуста: " + collection.isEmpty());
     }
 }
