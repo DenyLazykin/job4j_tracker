7b129b861c0f4dd136928d9e8574085e768b3c8d
Denis Lazykin <dlazykin856@gmail.com>
2022-11-12 12:31:48 +0300

    Интерфейс Collection [#504820 #257920].Демонстрация работы методов add, addAll.

---
+++ src/main/java/ru/job4j/collection/CollectionUsage.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionUsage {
+    public static void main(String[] args) {
+        Collection<String> collection = new ArrayList<>();
+        collection.add("one");
+        collection.add("two");
+        collection.add("three");
+        for (String str : collection) {
+            System.out.println(str);
+        }
+        Collection<String> list = new ArrayList<>();
+        list.addAll(collection);
+        for (String str : list) {
+            System.out.println(list);
+        }
+    }
+}
