95240723c9faca9bcdfc660dd9830a6f38bd9861
Denis Lazykin <dlazykin856@gmail.com>
2022-11-13 11:36:04 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #257940].Добавил модель справочника PhoneDictionary, с методами: добавить и найти

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,33 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+/**
+ * Модель справочника
+ */
+public class PhoneDictionary {
+    private ArrayList<Person> people = new ArrayList<>();
+
+    public void add(Person person) {
+        this.people.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, которые содержат key в любых полях
+     *
+     * @param key ключ поиска
+     * @return список пользователей, которые прошли проверку
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : people) {
+            if (person.getName().contains(key)
+                    || person.getSurname().contains(key)
+                    || person.getPhone().contains(key)
+                    || person.getAddress().contains(key)) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
