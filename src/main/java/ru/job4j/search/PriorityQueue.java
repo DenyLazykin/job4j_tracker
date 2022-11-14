b9e687e5099677816b7dcbcb589b9ca6e72813dc
Denis Lazykin <dlazykin856@gmail.com>
2022-11-14 09:41:58 +0300

    2. Очередь с приоритетом на LinkedList [#41670 #257997].Добавил класс PriorityQueue в котором описаны методы для добавления задачи и возврата элента из списка и его удалени

---
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод вставляет в нужную позицию элемент.
+     * Позиция определяется по полю priority.
+     * Для вставки используем add(int index, E value)
+     *
+     * @param task задачу
+     */
+    public void put(Task task) {
+        int index = 0;
+        for (Task element : tasks) {
+            if (element.getPriority() > task.getPriority()) {
+                break;
+            }
+            index++;
+        }
+        this.tasks.add(index, task);
+    }
+
+    public Task take() {
+        return tasks.poll();
+    }
+}
