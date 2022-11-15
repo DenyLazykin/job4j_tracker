79f5d3ea5031f1da3e5c2badc7b92c41050d6375
Denis Lazykin <dlazykin856@gmail.com>
2022-11-15 14:19:39 +0300

    0. Set, HashSet, Iterator. [#10095 #258675]

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,19 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>() {{
+            add("Lada");
+            add("BMW");
+            add("Volvo");
+            add("Toyota");
+            add("Toyota");
+            add("BMW");
+        }};
+        for (String auto : autos) {
+            System.out.println(auto);
+        }
+    }
+}
