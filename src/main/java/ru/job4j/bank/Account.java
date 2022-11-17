cb0f875c88630214de558cb1b6620846dd7bdf01
Denis Lazykin <dlazykin856@gmail.com>
2022-11-17 17:02:49 +0300

    3. Банковские переводы [#10038 #258762].Написал модель данных Account

---
+++ src/main/java/ru/job4j/bank/Account.java
@@ -0,0 +1,49 @@
+package ru.job4j.bank;
+
+import java.util.Objects;
+
+/**
+ * Модель данных банковского счета
+ */
+public class Account {
+    private String requisite;
+    private String balance;
+
+    public Account(String requisite, String balance) {
+        this.requisite = requisite;
+        this.balance = balance;
+    }
+
+    public String getRequisite() {
+        return requisite;
+    }
+
+    public void setRequisite(String requisite) {
+        this.requisite = requisite;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Account account = (Account) o;
+        return Objects.equals(requisite, account.requisite);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requisite);
+    }
+}
