9c6d06c76390d6b234353a10eb9a47b36baf0f59
Denis Lazykin <dlazykin856@gmail.com>
2022-11-17 16:54:51 +0300

    2. Преобразования List в Map. [#10093 #258741].Исправил метод в классе OrderConvert

--- src/main/java/ru/job4j/collection/OrderConvert.java
+++ src/main/java/ru/job4j/collection/OrderConvert.java
@@ -2,20 +2,19 @@ package ru.job4j.collection;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class OrderConvert {
     /**
      * Преобразовывает одну поллекцию в другую
      * Проходим по всем элементам списка orders и вставляем их в мапу
      *
-     * @param orders
-     * @return
+     * @param orders Список заявок
+     * @return Преобразованный список из листа в мапу
      */
     public static HashMap<String, Order> process(List<Order> orders) {
         HashMap<String, Order> map = new HashMap<>();
-        for (Map.Entry<String, Order> entry : map.entrySet()) {
-            map.put(entry.getKey(), entry.getValue());
+        for (Order order : orders) {
+            map.put(order.getNumber(), order);
         }
         return map;
     }
