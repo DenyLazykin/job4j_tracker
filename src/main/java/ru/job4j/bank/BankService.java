3d69bb1e5e6a200326dff6092049e00c3be8c73f
Denis Lazykin <dlazykin856@gmail.com>
2022-11-18 09:56:06 +0300

    3. Банковские переводы [#10038 #258762].Написал метод для добавления пользователя addUser в классе BankService

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,50 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Логика работы банка
+ * Поле users - соодержит всех пользователей системы с привязанными к ним счетами
+ */
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    /**
+     * Добавляет пользователя в систему
+     * putIfAbsent - добавляет пару в мапу, если ее там нет
+     * @param user Пользователя
+     */
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public boolean deleteUser(String passport) {
+        boolean result = false;
+        return result;
+    }
+
+    public  void addAccount(String passport, Account account) {
+
+    }
+
+    public User findByPassport(String passport) {
+
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean result = false;
+        return result;
+    }
+
+    public List<Account> getAccounts(User user) {
+        return users.get(user);
+    }
+}
