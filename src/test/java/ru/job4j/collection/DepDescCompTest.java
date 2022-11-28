9787ac9049d2cbc225bec67a3fda83238170a121
Denis Lazykin <dlazykin856@gmail.com>
2022-11-28 15:49:42 +0300

    2. Отсортировать департаменты [#34352 #266177].Написал тесты для компаратора

---
+++ src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DepDescCompTest {
+    @Test
+    public void compare() {
+        int result = new DepDescComp().compare(
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        assertThat(result).isGreaterThan(0);
+    }
+
+    @Test
+    public void whenUpDepartmentGoBefore() {
+        int result = new DepDescComp().compare(
+                "K2",
+                "K2/SK1"
+        );
+        assertThat(result).isLessThan(0);
+    }
+
+    @Test
+    public void whenFirsDepIsDifferentThenAsc() {
+        int result = new DepDescComp().compare(
+                "K2/SK1/SSK1",
+                "K1/SK1/SSK2"
+        );
+        assertThat(result).isLessThan(0);
+    }
+
+    @Test
+    public void whenFirstDepIsDifferentThenDesc() {
+        int result = new DepDescComp().compare(
+                "K1/SK1/SSK1",
+                "K2/SK1"
+        );
+        assertThat(result).isGreaterThan(0);
+    }
+}
