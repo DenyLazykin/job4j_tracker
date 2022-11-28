dfa207647fdd42be1b2b979cee636fbcdcf006ac
Denis Lazykin <dlazykin856@gmail.com>
2022-11-28 15:48:03 +0300

    2. Отсортировать департаменты [#34352 #266177].Написал метод для добавления подразделений в классе Departments

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+/**
+ * Класс для добавления пропущенных подразделений, и сортировки подразденелений
+ */
+public class Departments {
+    /**
+     * Метод для добавления пропущенных подразделений.
+     * Вначале определяем, какие элементы отсутсвуют в системе.
+     * Для этого каждую входящую строку разбиваем на одиночные элементы.
+     * Церез цикл складываем элементы, и добавляем их в множество
+     *
+     * @param deps Список строк
+     * @return Сформированный список
+     */
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> temp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                start += "".equals(start) ? el : "/" + el;
+                temp.add(start);
+            }
+        }
+        return new ArrayList<>(temp);
+    }
+}
