b4300066a28b9d0c187c635dd5740bc6b514b815
Denis Lazykin <dlazykin856@gmail.com>
2022-11-30 16:46:27 +0300

    2.2. Функциональный интерфейс [#247497 #266703]

--- src/main/java/ru/job4j/function/FunctionalInterface.java
+++ src/main/java/ru/job4j/function/FunctionalInterface.java
@@ -8,6 +8,7 @@ import java.util.function.*;
 
 public class FunctionalInterface {
     public static void main(String[] args) {
+        System.out.println("Вставляем элементы в карту с использованием biConsumer");
         Map<Integer, String> map = new HashMap();
         BiConsumer<Integer, String> biConsumer =
                 (i, s) -> map.put(i, s);
@@ -18,15 +19,19 @@ public class FunctionalInterface {
         biConsumer.accept(5, "five");
         biConsumer.accept(6, "six");
         biConsumer.accept(7, "seven");
+        System.out.println("Проверка элементов");
         BiPredicate<Integer, String> biPredicate = (i, s) -> (i % 2 == 0) || s.length() == 4;
         for (Integer i : map.keySet()) {
             if (biPredicate.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
         }
+        System.out.println("Создаем ArrayList из значений Map с помощью supplier");
         Supplier<List<String>> supplier = () -> new ArrayList<>(map.values());
         List<String> strings = supplier.get();
+        System.out.println("Вывод строк с помощью consumer");
         Consumer<String> consumer = (s) -> System.out.println(s);
+        System.out.println("Преобразование строк к строкам в верхнем регистре");
         Function<String, String> function = (s) -> s.toUpperCase();
         for (String s : strings) {
             consumer.accept(function.apply(s));
