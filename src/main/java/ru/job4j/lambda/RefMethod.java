88f0553588c8a91bbfbc71fd6bd9a34f5ab9e706
Denis Lazykin <dlazykin856@gmail.com>
2022-12-02 15:22:11 +0300

    3. Ссылки на методы [#70608].Задание

--- src/main/java/ru/job4j/lambda/RefMethod.java
+++ src/main/java/ru/job4j/lambda/RefMethod.java
@@ -1,18 +1,24 @@
 package ru.job4j.lambda;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
 public class RefMethod {
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+
     public static void main(String[] args) {
         List<String> names = Arrays.asList(
                 "Ivan",
-                "Den"
+                "Denis Lazykin"
         );
-        Consumer<String> consumer = System.out::println;
-        names.forEach(consumer);
-        Comparator<Integer> intCmp = Integer::compare;
+        Consumer<String> out = RefMethod::cutOut;
+        names.forEach(out);
     }
 }
