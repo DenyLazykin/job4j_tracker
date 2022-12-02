08e0e0392d45862fc5a81f59d0d4ab4bbbb4d351
Denis Lazykin <dlazykin856@gmail.com>
2022-12-02 15:15:31 +0300

    3. Ссылки на методы [#70608].Пример из лекции

---
+++ src/main/java/ru/job4j/lambda/RefMethod.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "Ivan",
+                "Den"
+        );
+        Consumer<String> consumer = System.out::println;
+        names.forEach(consumer);
+        Comparator<Integer> intCmp = Integer::compare;
+    }
+}
