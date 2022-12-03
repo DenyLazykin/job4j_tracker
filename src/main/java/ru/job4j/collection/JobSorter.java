ce72ea39e7f573aa1de5ef0402503419481c6839
Denis Lazykin <dlazykin856@gmail.com>
2022-12-03 11:00:36 +0300

    7. Функции высшего порядка [#24255].Задание из лекции

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -1,7 +1,6 @@
 package ru.job4j.collection;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,18 +12,12 @@ public class JobSorter {
                 new Job("Reboot server", 1),
                 new Job("X task", 0)
         );
+        jobs.sort(new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
-        Collections.sort(jobs);
+        Comparator<Job> comparatorName = Comparator.comparing(Job::getName);
+        Comparator<Job> comparatorPriority = Comparator.comparing(Job::getPriority);
+        Comparator<Job> comparatorComboNamePriority = comparatorName.thenComparing(comparatorPriority);
+        jobs.sort(comparatorComboNamePriority);
         System.out.println(jobs);
-        Collections.sort(jobs, new SortByNameJob());
-        System.out.println(jobs);
-        jobs.sort(new SortDescByNameJob());
-        System.out.println(jobs);
-        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
-        System.out.println(jobs);
-        Comparator<Job> comb = new JobDescByNameLn()
-                .thenComparing(new JobDescByName())
-                .thenComparing(new JobDescByPriority());
-        jobs.sort(comb);
-    }
+     }
 }
