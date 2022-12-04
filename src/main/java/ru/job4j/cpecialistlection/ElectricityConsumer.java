79c16fc5c151d18e2f895071a52fa76a16f50fd2
Denis Lazykin <dlazykin856@gmail.com>
2022-12-04 16:13:21 +0300

    Лекция специалист по лямбдам. Написал интерфейс связывающий обьекты

---
+++ src/main/java/ru/job4j/cpecialistlection/ElectricityConsumer.java
@@ -0,0 +1,8 @@
+package ru.job4j.cpecialistlection;
+
+/**
+ * Интерфейс для связи выключателя с потребителями электрической сети
+ */
+public interface ElectricityConsumer {
+    void electricityOn();
+}
