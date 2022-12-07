62d1c91d43e6811309568a62c1f7faf1c7019d2a
Denis Lazykin <dlazykin856@gmail.com>
2022-12-07 15:47:33 +0300

    9. Генерирование колоды карт. FlatMap [#504873]. Пример с лекции

--- src/main/java/ru/job4j/stream/school/School.java
+++ src/main/java/ru/job4j/stream/school/School.java
@@ -3,6 +3,7 @@ package ru.job4j.stream.school;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class School {
     /**
@@ -15,6 +16,26 @@ public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predicate) {
         return students.stream()
                 .filter(predicate)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Фильтрует и преобразует список студентов по среднему балу
+     * Убираем из потока пустые элементы,
+     * поток сортируем,
+     * потом фильтруем по уровню,
+     * сохраняем в список
+     *
+     * @param students Список втудентов
+     * @param bound    Балл студентов
+     * @return Преобразованный список
+     */
+    public List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .flatMap(Stream::ofNullable)
+                .sorted()
+                .filter(student ->
+                        student.score() > bound)
                 .collect(Collectors.toList());
     }
 }
