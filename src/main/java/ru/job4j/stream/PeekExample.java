d835a546849400f6649e82fb19b9abd28ae1b1ac
Denis Lazykin <dlazykin856@gmail.com>
2022-12-08 10:21:50 +0300

    11. Метод peek() [#504924 #368789].

---
+++ src/main/java/ru/job4j/stream/PeekExample.java
@@ -0,0 +1,34 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Изучаем промежуточную операцию peek().
+ * (22) Создаем лиси объектов StringBuilder names,
+ * содержимое которых будем изменять в потоке
+ * (27) Создаем лист StringBuilder editNames в котором будем изменять элементы.
+ * (28) Запускаем поток.
+ * (29) .peek((el -> el.append(" (student Job4j)"))) - изменяет элементы,
+ * применяя append() к каждому объекту потока с помощью peek()
+ * Так как peek() - промежуточный метод,
+ * то после изменения эти обьекты модно использовать дальше в потоке,
+ * применяя другие промежуточные методы.
+ * (30) Сортируем элементы.
+ * (31) Собираем все элементы потока в лист
+ */
+public class PeekExample {
+    public static void main(String[] args) {
+        List<StringBuilder> names = Arrays.asList(
+                new StringBuilder("Den"),
+                new StringBuilder("Ivan"),
+                new StringBuilder("Elena")
+        );
+        List<StringBuilder> editNames = names
+                .stream()
+                .peek((el -> el.append(" (student Job4j)")))
+                .sorted()
+                .toList();
+        System.out.println(editNames);
+    }
+}
