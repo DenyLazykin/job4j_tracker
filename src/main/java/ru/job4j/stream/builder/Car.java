ad7a1b649f21040fc206e6f598ac635159c3e04a
Denis Lazykin <dlazykin856@gmail.com>
2022-12-10 10:57:00 +0300

    18. Шаблоны проектирования в Stream API [#310012]

---
+++ src/main/java/ru/job4j/stream/builder/Car.java
@@ -0,0 +1,77 @@
+package ru.job4j.stream.builder;
+
+import java.time.LocalDate;
+
+public class Car {
+    private String brand;
+    private String model;
+    private LocalDate created;
+    private double volume;
+    private String color;
+
+    @Override
+    public String toString() {
+        return String.format("Car(brand=%s , model=%s , created=%s , volume=%s , color=%s)",
+                this.brand,
+                this.model,
+                this.created,
+                this.volume,
+                this.color);
+    }
+
+    static class Builder {
+        private String brand;
+        private String model;
+        private LocalDate created;
+        private double volume;
+        private String color;
+
+        Builder buildBrand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        Builder buildModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        Builder buildCreated(LocalDate created) {
+            this.created = created;
+            return this;
+        }
+
+        Builder buildVolume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+
+        Builder buildColor(String color) {
+            this.color = color;
+            return this;
+        }
+
+        Car build() {
+            Car car = new Car();
+            car.brand = brand;
+            car.model = model;
+            car.created = created;
+            car.volume = volume;
+            car.color = color;
+            return car;
+        }
+    }
+}
+
+class CarElantra {
+    public static void main(String[] args) {
+        Car carElantra = new Car.Builder()
+                .buildBrand("Hyundai")
+                .buildModel("Elantra")
+                .buildCreated(LocalDate.of(2008, 1, 1))
+                .buildVolume(2.0)
+                .buildColor("Black")
+                .build();
+        System.out.println(carElantra);
+    }
+}
