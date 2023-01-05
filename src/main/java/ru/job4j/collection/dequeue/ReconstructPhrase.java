bd5f23160f77609f6fe06fccdc09f4ec322063df
Denis Lazykin <dlazykin856@gmail.com>
2023-01-05 16:53:40 +0300

    2. Интерфейс Deque [#504943].

---
+++ src/main/java/ru/job4j/collection/dequeue/ReconstructPhrase.java
@@ -0,0 +1,38 @@
+package ru.job4j.collection.dequeue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+    private final Deque<Character> descendingElements;
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int size = evenElements.size();
+        for (int i = 0; i < size; i++) {
+            if (i % 2 == 0) {
+                stringBuilder.append(evenElements.pollFirst());
+            } else {
+                evenElements.pollFirst();
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (!descendingElements.isEmpty()) {
+            stringBuilder.append(descendingElements.pollLast());
+        }
+        return stringBuilder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
