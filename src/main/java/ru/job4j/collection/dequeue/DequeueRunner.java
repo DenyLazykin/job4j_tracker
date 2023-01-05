c9515af1aba86c79f549b2e340d345e0493d4e0c
Denis Lazykin <dlazykin856@gmail.com>
2023-01-05 16:18:04 +0300

    2. Интерфейс Deque [#504943].Пример с лекции.

---
+++ src/main/java/ru/job4j/collection/dequeue/DequeueRunner.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection.dequeue;
+
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class DequeueRunner {
+    public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<>();
+        deque.add("second");
+        deque.addFirst("first");
+        deque.addLast("third");
+        Iterator<String> iterator = deque.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        Iterator<String> descendingIterator = deque.descendingIterator();
+        while (descendingIterator.hasNext()) {
+            System.out.println(descendingIterator.next());
+        }
+        System.out.println(deque);
+        System.out.println(deque.pop());
+        System.out.println(deque.poll());
+        System.out.println(deque.pollFirst());
+        System.out.println(deque.pollLast());
+    }
+}
